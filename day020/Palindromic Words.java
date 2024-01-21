96722820147fccca4c11093909da47ce3bf78faa
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-01-21 23:35:22 +0530

    solved day019/365

---
+++ day020/Palindromic Words.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int solve(String A) {
+        String[] words = A.split("\\s+");
+        int palindromicCount = 0;
+        for (String word : words) {
+            if (isPalindrome(word)) {
+                palindromicCount++;
+            }
+        }
+
+        return palindromicCount;
+    }
+
+    private static boolean isPalindrome(String word) {
+        int start = 0, end = word.length() - 1;
+        while (start < end) {
+            if (word.charAt(start) != word.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
