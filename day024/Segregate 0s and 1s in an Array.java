9438bd690e8a337187c8c43bbdd8ec7ceaa8f0d5
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-01-25 20:28:26 +0530

    solved day024/365

--- day024/Segregate 0s and 1s in an Array.java
+++ day024/Segregate 0s and 1s in an Array.java
@@ -2,12 +2,13 @@ public class Solution {
     public int[] solve(int[] A) {
         int left = 0;
         int right = A.length - 1;
-
+        
         while (left < right) {
+            //check sorted from left
             while (A[left] == 0 && left < right) {
                 left++;
             }
-
+            //check sorted from right
             while (A[right] == 1 && left < right) {
                 right--;
             }
