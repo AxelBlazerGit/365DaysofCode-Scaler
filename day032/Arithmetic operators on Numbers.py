478bb0a6b86cfedcf23398193c943af5a9b3c0d1
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-02-02 08:41:13 +0530

    solve day032/365

---
+++ day032/Arithmetic operators on Numbers.py
@@ -0,0 +1,17 @@
+def main():
+    A = int(raw_input())
+    B = int(raw_input())
+    x = float(A) / B
+
+    print(A+B)
+    print(A-B)
+    print(A*B)
+    print(A//B)
+    print(x)
+    print(A%B)
+    print(A**B)
+    # Print seven lines as described above
+    return 0
+
+if __name__ == '__main__':
+    main()
