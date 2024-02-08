b05289ae3e5f290035a2c459dd418dc9b6ea2b61
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-02-08 23:05:22 +0530

    solve day038/365

---
+++ day038/String Operations.py
@@ -0,0 +1,15 @@
+def main():
+    S = raw_input()
+    print(len(S))
+    print(S.index('a'))
+    print(S[::2])
+    #Print length of the string S
+    
+    #Print the first occurence of the letter 'a' in S
+    #Note it is guaranteed that letter a is present in the string S
+    
+    #Print all the characters with even index in S
+    return 0
+
+if __name__ == '__main__':
+    main()
