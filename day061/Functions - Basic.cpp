09784ffb7b8750e75ed8e69e7cbfb57e1ba4bb7c
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-03-02 21:51:55 +0530

    Create Functions - Basic.cpp

---
+++ day061/Functions - Basic.cpp
@@ -0,0 +1,19 @@
+/*
+#include<iostream>
+using namespace std;
+*/
+
+// Your Code goes here
+int compute(int A,int B){ return (A*A+B*B);}
+
+
+
+
+/*
+int main()  {
+    int A, B;
+    cin>>A>>B;
+    cout<<compute(A, B)<<endl;
+    return 0;
+}
+*/
