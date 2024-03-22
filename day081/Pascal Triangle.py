84f4fd3bab08b26faea97c2e134d8464cccd2c25
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-03-22 23:34:56 +0530

    solve day081/365

---
+++ day081/Pascal Triangle.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param A : integer
+    # @return a list of list of integers
+    def solve(self, A):
+        if A <= 0:
+            return []
+
+        triangle = []
+        for i in range(A):
+            row = [1] * (i + 1)
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            triangle.append(row)
+
+        return triangle
