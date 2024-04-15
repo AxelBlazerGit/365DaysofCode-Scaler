78698f2476ef3dbd927353f35a9792c44a4f0632
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-04-16 01:39:32 +0530

    solve day106/365

---
+++ day106/Power of 2.py
@@ -0,0 +1,7 @@
+class Solution:
+    # @param A : string
+    # @return an integer
+    def power(self, A):
+        if A == '1':
+            return 0
+        return 1 if int(A)&(int(A)-1)==0 else 0
