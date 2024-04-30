c982284a52dbad57d8e7713400fef6781cc19007
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-04-30 23:05:57 +0530

    solve day120/365

---
+++ day120/Greatest Common Divisor.py
@@ -0,0 +1,8 @@
+class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @return an integer
+    def gcd(self, a, b):
+        if b == 0:
+            return a
+        return self.gcd(b, a % b)
